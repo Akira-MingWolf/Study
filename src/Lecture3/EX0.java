ccd570674ee79be054ca5dea361f840a7d8149b4
Akira_MingWolf <chankaming56@gmail.com>
2022-10-02 19:46:05 +0800

    1

--- src/Lecture3/EX0.java
+++ src/Lecture3/EX0.java
@@ -1,51 +1,42 @@
 package Lecture3;
-import java.util.Scanner;
+
 public class EX0 {
     public static void main (String[] args) {
-//input
-        Scanner input = new Scanner(System.in);
 //Q1
-        System.out.print("What is your score? ");
-        int a = input.nextInt();
+        int a = 40;
         String A;
         A = a < 50 ? "fail" : "pass";
-        System.out.printf(a + " is " + A);
+        System.out.println(a + " is " + A);
 //Q2
-        System.out.print("\nWhat is the price? ");
-        double b = input.nextDouble();
+        double b = 5.0;
         String B;
         B = b < 10.5 ? "cheap" : "expensive";
-        System.out.printf(b + " is " + B);
+        System.out.println(b + " is " + B);
 //Q3
-        System.out.print("\nWhat is the temperature? ");
-        int c = input.nextInt();
+        int c = 10;
         String C;
         C = c > 28 ? "hot" : "warm";
-        System.out.printf(c + " is " + C);
+        System.out.println(c + " is " + C);
 //Q4
-        System.out.print("\nWhat is the label? ");
-        String d = input.next();
+        String d = "-";
         String D;
         D = (d.equals("+")) ? "+1" : "-1";
-        System.out.printf(d + " is " + D);
+        System.out.println(d + " is " + D);
 //Q5
-        System.out.print("\nWhat is the word? ");
-        String e = input.next();
+        String e = "not_good";
         String E;
         E = (e.equals("ok")) ? "y" : "n";
-        System.out.printf(e + " is " + E);
+        System.out.println(e + " is " + E);
 //Q6
-        System.out.print("\nWhat is the gender? ");
-        String f = input.next();
+        String f = "F";
         String F;
         F = (f.equals("M")) ? "Male" : "Female";
-        System.out.printf(f + " is " + F);
+        System.out.println(f + " is " + F);
 //Q7
-        System.out.print("\nWhat is number");
-        double g = input.nextDouble();
+        double g = 3.5;
         String G;
         double difference = g - Math.round(g);
         G = difference == 0 ? "integer" : "decimal number";
-        System.out.printf(g + " is " + G);
+        System.out.println(g + " is " + G);
     }
 }
