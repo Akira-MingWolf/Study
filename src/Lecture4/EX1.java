d9c3ebee26baa9f688a7d69e02ff7cb9d504f3dd
Akira_MingWolf <chankaming56@gmail.com>
2022-10-03 09:32:43 +0800

    1

--- src/Lecture4/EX1.java
+++ src/Lecture4/EX1.java
@@ -5,63 +5,67 @@ public class EX1 {
         //while loop
         System.out.println("While-loop");
         System.out.print("Integers : ");
-        int whileLoopIntegerCount = 10;
-        while (whileLoopIntegerCount <= 15) {
+        int whileLoopIntegerCount = 10; // Initialization
+        while (whileLoopIntegerCount <= 15) { // looping condition
             System.out.print(whileLoopIntegerCount + " ");
-            whileLoopIntegerCount ++;
+            whileLoopIntegerCount ++; // Increment
         }
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.print("Even : ");
-        int whileLoopEvenCount = 10;
-        while (whileLoopEvenCount <= 15) {
+        int whileLoopEvenCount = 10; // Initialization
+        while (whileLoopEvenCount <= 15) { // looping condition
             System.out.print(whileLoopEvenCount + " ");
             whileLoopEvenCount += 2;
         }
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.print("Odd : ");
-        int whileLoopOddCount = 11;
-        while (whileLoopOddCount <= 15) {
+        int whileLoopOddCount = 11; // Initialization
+        while (whileLoopOddCount <= 15) { // looping condition
             System.out.print(whileLoopOddCount + " ");
             whileLoopOddCount += 2;
         }
+
+
         // do-while loop
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.println("Do-while loop");
         System.out.print("Integers : ");
-        int doWhileLoopIntegerCount = 10;
+        int doWhileLoopIntegerCount = 10; // Initialization
         do {
             System.out.print(doWhileLoopIntegerCount + " ");
             doWhileLoopIntegerCount ++;
-        } while (doWhileLoopIntegerCount <= 15);
-        System.out.println(); //
+        } while (doWhileLoopIntegerCount <= 15); // looping condition
+        System.out.println(); // skip line
         System.out.print("Even : ");
-        int doWhileLoopEvenCount = 10;
+        int doWhileLoopEvenCount = 10; // Initialization
         do {
             System.out.print(doWhileLoopEvenCount + " ");
             doWhileLoopEvenCount += 2;
-        } while (doWhileLoopEvenCount <= 15);
-        System.out.println(); //
+        } while (doWhileLoopEvenCount <= 15); // looping condition
+        System.out.println(); // skip line
         System.out.print("Odd : ");
-        int doWhileLoopOddCount = 11;
+        int doWhileLoopOddCount = 11; // Initialization
         do {
             System.out.print(doWhileLoopOddCount + " ");
             doWhileLoopOddCount += 2;
-        } while (doWhileLoopOddCount <= 15);
+        } while (doWhileLoopOddCount <= 15); // looping condition
+
+
         // for loop
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.println("For-loop");
         System.out.print("Integers : ");
         int forLoopIntegerCount;
         for (forLoopIntegerCount = 10       ; forLoopIntegerCount <=15      ; forLoopIntegerCount ++) {
             System.out.print(forLoopIntegerCount + " ");
         }
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.print("Even : ");
         int forLoopEvenCount;
         for (forLoopEvenCount = 10      ; forLoopEvenCount <= 15        ; forLoopEvenCount += 2) {
             System.out.print(forLoopEvenCount + " ");
         }
-        System.out.println(); //
+        System.out.println(); // skip line
         System.out.print("Odd : ");
         int forLoopOddCount;
         for (forLoopOddCount = 11       ; forLoopOddCount <=15      ; forLoopOddCount += 2) {
